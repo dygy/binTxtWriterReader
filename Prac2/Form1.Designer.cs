861ff8e09811bf2d723cb6e41ef78d14bdb4886d
Dygy <Yukimurachris@gmail.com>
2019-03-11 20:23:18 +0300

    Добавьте файлы проекта.

---
+++ Prac2/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Prac2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.addItem = new System.Windows.Forms.Button();
+            this.ReadTxt = new System.Windows.Forms.RichTextBox();
+            this.ReadBin = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Date";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Region";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Price";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(75, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(75, 116);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "RU",
+            "DE",
+            "UA",
+            "US",
+            "UK",
+            "JP",
+            "CH",
+            "HK"});
+            this.comboBox1.Location = new System.Drawing.Point(75, 162);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // addItem
+            // 
+            this.addItem.Location = new System.Drawing.Point(26, 199);
+            this.addItem.Name = "addItem";
+            this.addItem.Size = new System.Drawing.Size(170, 23);
+            this.addItem.TabIndex = 9;
+            this.addItem.Text = "Add Item";
+            this.addItem.UseVisualStyleBackColor = true;
+            this.addItem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ReadTxt
+            // 
+            this.ReadTxt.Location = new System.Drawing.Point(217, 14);
+            this.ReadTxt.Name = "ReadTxt";
+            this.ReadTxt.Size = new System.Drawing.Size(1022, 208);
+            this.ReadTxt.TabIndex = 10;
+            this.ReadTxt.Text = "";
+            // 
+            // ReadBin
+            // 
+            this.ReadBin.Location = new System.Drawing.Point(26, 241);
+            this.ReadBin.Name = "ReadBin";
+            this.ReadBin.Size = new System.Drawing.Size(1213, 334);
+            this.ReadBin.TabIndex = 11;
+            this.ReadBin.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1285, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "TXT file";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1288, 418);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "BIN file";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1522, 587);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ReadBin);
+            this.Controls.Add(this.ReadTxt);
+            this.Controls.Add(this.addItem);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button addItem;
+        private System.Windows.Forms.RichTextBox ReadTxt;
+        private System.Windows.Forms.RichTextBox ReadBin;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Prac2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Prac2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(Application.StartupPath+"\\Memory"+"\\"+textBox1.Text+".txt" );
+            sw.WriteLine(label1.Text + " " + textBox1.Text);
+            sw.WriteLine(label2.Text + " " + textBox2.Text);
+            sw.WriteLine(label4.Text + " " + textBox3.Text);
+            sw.WriteLine(label3.Text + " " + comboBox1.Text);
+            sw.Close();
+            FileStream fs = new FileStream(Application.StartupPath + "\\Memory" + "\\" + textBox1.Text + ".bin", FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(label1.Text + " " + textBox1.Text);
+            bw.Write(label2.Text + " " + textBox2.Text);
+            bw.Write(label4.Text + " " + textBox3.Text);
+            bw.Write(label3.Text + " " + comboBox1.Text);
+            bw.Close();
+
+            StreamReader sr = new StreamReader (Application.StartupPath + "\\Memory" + "\\" + textBox1.Text + ".txt");
+            ReadTxt.Text = sr.ReadToEnd(); 
+            sr.Close();
+
+            FileStream fs1 = new FileStream(Application.StartupPath + "\\Memory" + "\\" + textBox1.Text + ".bin", FileMode.Open);
+            BinaryReader br = new BinaryReader(fs1);
+            ReadBin.Text = br.ReadString() + "\n" + br.ReadString() + "\n"+ br.ReadString() + "\n" + br.ReadString();
+            fs1.Close();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
